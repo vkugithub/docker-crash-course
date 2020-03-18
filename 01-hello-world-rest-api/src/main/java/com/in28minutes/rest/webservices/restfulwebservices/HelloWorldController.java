04c385dfcd84f334e30f73c4070281fa97ed7fe0
vkumar.kumar <vkumar112@sapient.com>
2020-03-18 10:09:56 +0530

    Build docker image using JIB plugin and docker configuration

--- 01-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ 01-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -16,7 +16,7 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldBean() {
 		// throw new RuntimeException("Some Error has Happened! Contact Support at
 		// ***-***");
-		return new HelloWorldBean("Hello World V3");
+		return new HelloWorldBean("Hello World V4");
 	}
 
 	/// hello-world/path-variable/in28minutes
